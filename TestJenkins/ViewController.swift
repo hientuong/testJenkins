339130638ec17b4a683fc8ad0966260e45e60f29
Hien Tuong <tuongnh1@tpb.com.vn>
2019-09-21 13:38:26 +0700

    First commit

---
+++ TestJenkins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestJenkins
+//
+//  Created by Hien Tuong on 9/21/19.
+//  Copyright © 2019 Đỗ Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
